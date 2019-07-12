a603b94e09e96f94402a893dc11296a9a3b155ed
Falanke21 <1065314944@qq.com>
2019-07-12 16:15:58 -0400

    add files for a3

---
+++ A3/heuristics.py
@@ -0,0 +1,34 @@
+#Look for #IMPLEMENT tags in this file. These tags indicate what has
+#to be implemented.
+
+import random
+'''
+This file will contain different variable ordering heuristics to be used within
+bt_search.
+
+var_ordering == a function with the following template
+    var_ordering(csp)
+        ==> returns Variable 
+
+    csp is a CSP object---the heuristic can use this to get access to the
+    variables and constraints of the problem. The assigned variables can be
+    accessed via methods, the values assigned can also be accessed.
+
+    var_ordering returns the next Variable to be assigned, as per the definition
+    of the heuristic it implements.
+
+val_ordering == a function with the following template
+    val_ordering(csp,var)
+        ==> returns [Value, Value, Value...]
+    
+    csp is a CSP object, var is a Variable object; the heuristic can use csp to access the constraints of the problem, and use var to access var's potential values. 
+
+    val_ordering returns a list of all var's potential values, ordered from best value choice to worst value choice according to the heuristic.
+
+'''
+
+def ord_mrv(csp):
+    #IMPLEMENT
+
+def val_lcv(csp,var):
+    #IMPLEMENT
