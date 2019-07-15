e2dd55ebd4a960d579e18752b0cd2d6b45e9649c
Falanke21 <1065314944@qq.com>
2019-07-14 21:53:04 -0400

    implemented nary_ad_grid

--- A3/kenken_csp.py
+++ A3/kenken_csp.py
@@ -34,13 +34,83 @@ import itertools
 
 
 def binary_ne_grid(kenken_grid):
-    ##IMPLEMENT
+    # n = kenken_grid[0][0]
+    # variables = []
+    # two_d_array = []
+    #
+    # for i in range(n):
+    #     two_d_array.append([])
+    #     for j in range(n):
+    #         var = Variable(str(i + 1) + str(j + 1), range(1, n + 1))
+    #         variables.append(var)
+    #         two_d_array[i].append(var)
+    #     con = Constraint("Row " + str(i), two_d_array[i])
+    #
+    #     varDoms = []
+    #     for v in two_d_array[i]:
+    #         varDoms.append(v.domain())
+    #     for t in itertools.product(*varDoms):
+    #         if lambda t:
+    #
+    # csp = CSP("kenken", variables)
+    #
+    #
+    # return csp, two_d_array
     pass
 
+def all_diff(t):
+    """
+    Helper for nary_ad_grid
+    """
+    if len(set(t)) == len(t):
+        return True
+    return False
 
 def nary_ad_grid(kenken_grid):
-    ##IMPLEMENT
-    pass
+    n = kenken_grid[0][0]
+    variables = []
+    two_d_array = []
+
+    # construct variables
+    for i in range(n):
+        two_d_array.append([])
+        for j in range(n):
+            var = Variable(str(i + 1) + str(j + 1), range(1, n + 1))
+            variables.append(var)
+            two_d_array[i].append(var)
+
+    csp = CSP("kenken", variables)
+
+    # add row constrains
+    for i in range(n):
+        row_con = Constraint("Row " + str(i + 1), two_d_array[i])
+        varDoms = []
+        sat_tuples = []
+        for v in two_d_array[i]:
+            varDoms.append(v.domain())
+        for t in itertools.product(*varDoms):
+            if all_diff(t):
+                sat_tuples.append(t)
+        row_con.add_satisfying_tuples(sat_tuples)
+        csp.add_constraint(row_con)
+
+    # add column constrains
+    for j in range(n):
+        curr_variables = []
+        for i in range(n):
+            curr_variables.append(two_d_array[i][j])
+        col_con = Constraint("Col " + str(j + 1), curr_variables)
+        varDoms = []
+        sat_tuples = []
+        for v in curr_variables:
+            varDoms.append(v.domain())
+        for t in itertools.product(*varDoms):
+            if all_diff(t):
+                sat_tuples.append(t)
+        col_con.add_satisfying_tuples(sat_tuples)
+        csp.add_constraint(col_con)
+
+    return csp, two_d_array
 
 
 def kenken_csp_model(kenken_grid):
